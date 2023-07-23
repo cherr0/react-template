ff1976640ebf34971cebb06df695646441a53598
Peter Park <adsl1664@gmail.com>
2023-07-23 19:01:36 +0900

    feat: 패키지 세팅 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,45 @@
 module.exports = {
-    env: {
-        browser: true,
-        node: true
-    },
-    extends: ['plugin:import/recommended', 'plugin:import/typescript'],
-    plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
-    settings: {
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.ts', '.tsx']
-        },
-        'import/resolver': {
-            typescript: {
-                alwaysTryTypes: true
-            }
-        }
-    },
-    rules: {
-        'react/function-component-definition': [
-            'warn',
-            {
-                namedComponents: 'arrow-function'
-            }
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['react-app'],
+  plugins: ['react-hooks', 'simple-import-sort'],
+  rules: {
+    'react/function-component-definition': [
+      'warn',
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    'simple-import-sort/exports': 'warn',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
         ],
-        'react-hooks/exhaustive-deps': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        'simple-import-sort/exports': 'warn',
-        'import/order': [
-            'warn',
-            {
-                groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
-                pathGroups: [
-                    {
-                        pattern: 'react',
-                        group: 'external',
-                        position: 'before'
-                    }
-                ],
-                alphabetize: {
-                    order: 'asc',
-                    caseInsensitive: true
-                },
-                'newlines-between': 'always'
-            }
-        ]
-    },
-    ignorePatterns: [
-        '**/*.js',
-        '**/*.json',
-        'node_modules',
-        'public',
-        'styles',
-        '.next',
-        'coverage',
-        'dist'
-    ]
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+  ignorePatterns: [
+    '**/*.js',
+    '**/*.json',
+    'node_modules',
+    'public',
+    'styles',
+    '.next',
+    'coverage',
+    'dist',
+  ],
 }
---
+++ craco.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const resolve = (arg) => path.resolve(__dirname, arg)
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': resolve('src')
+    }
+  }
+}
