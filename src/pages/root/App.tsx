91299f44a1a9e2984297e1e9f429859c9c52b92c
Peter Park <adsl1664@gmail.com>
2023-07-24 11:33:17 +0900

    feat: react-query-devtools 정보 추가

--- src/pages/root/App.tsx
+++ src/pages/root/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
@@ -21,6 +22,7 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <GlobalStyle />
+        <ReactQueryDevtools />
         <RouterProvider router={router} />
       </RecoilRoot>
     </QueryClientProvider>
